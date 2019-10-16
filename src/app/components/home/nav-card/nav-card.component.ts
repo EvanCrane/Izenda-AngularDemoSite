c8449a9c82f9698bb1267590f22858712f809b20
EvanCrane <evanf.crane@gmail.com>
2019-10-16 13:32:59 -0400

    Added navcard component

---
+++ src/app/components/home/nav-card/nav-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-card',
+  templateUrl: './nav-card.component.html',
+  styleUrls: ['./nav-card.component.css']
+})
+export class NavCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
