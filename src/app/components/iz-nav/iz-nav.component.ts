bac0b662324d4e1de84130713d4e80aee0e45ff6
EvanCrane <evanf.crane@gmail.com>
2019-10-17 16:42:58 -0400

    Updated iz-nav

--- src/app/components/iz-nav/iz-nav.component.ts
+++ src/app/components/iz-nav/iz-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-iz-nav',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IzNavComponent implements OnInit {
 
-  constructor() { }
+  currentUser: Observable<string>;
+  isAuthenticated: Observable<boolean>;
+  clicked: boolean;
+
+  constructor(private router: Router, private authService: AuthenticationService) {
+    this.currentUser = authService.currentUser();
+    this.isAuthenticated = authService.isAuthenticated();
+    this.clicked = this.clicked === undefined ? false : true;
+  }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+
+  setClicked(val: boolean): void {
+    this.clicked = val;
   }
 
 }
